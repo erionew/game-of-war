1f8b853f5ead71851f19ccd413f7487fc7803e1b
Erione White <erionewhite1999@gmail.com>
2022-12-15 19:36:36 -0600

    implemented shuffle fn

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ class Card {
 
 class Deck {
     constructor(length, cards){
-        this.length = length;
+        this.length = 52;
         this.cards = cards;
 
         let suits = ['hearts', 'spades', 'clubs', 'diamonds']
@@ -26,7 +26,34 @@ class Deck {
         return this.cards[Math.floor(Math.random() * this.length)]
     }
 
+    shuffle(){
+        for (let i = this.cards.length - 1; i > 0; i--) {
+   
+            let j = Math.floor(Math.random() * (i + 1));
+                        
+            let temp = this.cards[i];
+            this.cards[i] = this.cards[j];
+            this.cards[j] = temp;
+        }
+
+        return this.cards
+    }
+
 }
 
-let newDeck = new Deck(52,'','')
-console.log(newDeck.draw())
+class Player{
+    constructor(deck){
+        this.deck = deck
+    }
+}
+
+let newDeck = new Deck()
+
+newDeck.shuffle(newDeck)
+
+console.log(newDeck)
+
+
+
+// let player1 = new Player(newDeck.cards.splice(newDeck.cards.length / 2))
+// console.log(player1)
